91f3c4fd244d829efac5143354daa688be614f77
Emre Ergun <emreergun@engramsoft.com>
2023-08-21 23:37:15 +0300

    structs completed with impl

--- week2/structs/src/main.rs
+++ week2/structs/src/main.rs
@@ -4,21 +4,43 @@ struct Product {
     in_stock: bool,
 }
 
-fn main() {
-    let mut book = Product {
-        name: String::from("Book"),
-        price: 28.85,
-        in_stock: true,
-    };
+impl Product {
+    fn new(name: String, price: f32) -> Product {
+        Product {
+            name,
+            price,
+            in_stock: true,
+        }
+    }
+    fn calculate_sales_tax(&self) -> f32 {
+        self.price * 0.1
+    }
 
-    let price = book.price;
+    fn set_price(&mut self, price: f32) {
+        self.price = price;
+    }
+
+    fn buy(self) -> i32 {
+        let name = self.name;
+        println!("The book {name} is bought");
+        123
+    }
+}
+
+fn main() {
+    let mut book = Product::new(
+        String::from("Book"),
+        28.85,
+    );
     book.in_stock = false;
 
-    let sales_tax = calculate_sales_tax(&book);
+    let sales_tax = book.calculate_sales_tax();
     println!("sales tax: {}", sales_tax);
     
+    book.set_price(23.25);
+    println!("sales tax: {}", book.calculate_sales_tax());
+
+    book.buy();
 }
 
-fn calculate_sales_tax(product: &Product) -> f32 {
-    product.price * 0.1
-}
+
