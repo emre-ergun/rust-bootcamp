65e57b2edba1e1ed30d63730355384b2a9947cfe
Emre Ergun <emreergun@engramsoft.com>
2023-08-23 22:17:11 +0300

    external dependecies added into modules section and completed

--- week3/auth_service/src/auth_utils.rs
+++ week3/auth_service/src/auth_utils.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables, dead_code)]
+
 pub fn login(creds: models::Credentials) {
     //authenticate
     crate::database::get_user();
--- week3/auth_service/src/lib.rs
+++ week3/auth_service/src/lib.rs
@@ -1,12 +1,18 @@
-mod database;
-mod auth_utils;
+#![allow(unused_variables, dead_code)]
+use rand::prelude::*;
+use std::time::Duration;
+use std::thread;
 
+pub mod database;
+mod auth_utils;
 
 pub use auth_utils::models::Credentials;
 use database::Status;
 
-
 pub fn authenticate(creds: Credentials) {
+    let timeout = thread_rng().gen_range(1..5);
+    thread::sleep(Duration::from_secs(timeout));
+    println!("timeout: {timeout}");
     if let Status::Connected = database::connect_to_database() {
         auth_utils::login(creds);
     }
