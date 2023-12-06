7bf86ef5c048276b3fa4ac8b9dbd94d40e528478
Emre Ergun <emreergun@engramsoft.com>
2023-12-06 17:33:32 +0300

    impl block added to struct example

--- beg_week3/structs/src/main.rs
+++ beg_week3/structs/src/main.rs
@@ -4,17 +4,41 @@ struct Product {
     in_stock: bool,
 }
 
+impl Product {
+    fn new(name: String, price: f32) -> Product {
+        Product { name: name, price: price, in_stock: true }
+    }
+    // associated function
+    fn get_default_sales_tax() -> f32 {
+        0.1
+    }
+
+    fn calculate_sales_tax(&self) -> f32 {
+        self.price * Product::get_default_sales_tax()
+    }
+
+    fn set_price(&mut self, price: f32) {
+        self.price = price;
+    }
+
+    fn buy(self) -> i32 {
+        let name = self.name;
+        println!("{name} is bought!");
+        let bill_number = 123;
+        bill_number
+    }
+}
+
 fn main() {
-    let mut book = Product {
-        name: String::from("Book"),
-        price: 28.85,
-        in_stock: true,
-    };
+    let mut book = Product::new(String::from("Book"), 28.85);
 
-    let sale_tax = calculate_sales_tax(&book);
+    println!("Default sales tax: {}", Product::get_default_sales_tax());
+    let sale_tax = book.calculate_sales_tax();
     println!("sales tax: {sale_tax}");
+    
+    book.set_price(32.25);
+    let sale_tax = book.calculate_sales_tax();
+    println!("sales tax: {sale_tax}");
+    book.buy();
 }
 
-fn calculate_sales_tax(product: &Product) -> f32 {
-    product.price * 0.1
-}
