78021ea13b26f12aeeea60fe84dd3a4ad3a58d9a
Emre Ergun <emreergun@engramsoft.com>
2023-12-21 02:29:13 +0300

    procedural macros - attribute like example added

--- adv_week2/procedural_macros/src/main.rs
+++ adv_week2/procedural_macros/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use procedural_macros::*;
 
 trait Log {
@@ -14,21 +15,34 @@ struct Database {
 
 impl Database {
     fn new(url: String) -> Database {
-        Database {url, connections: 99}
+        Database { url, connections: 99 }
     }
-
     fn connect(&mut self) {
         self.info(format!("new connection to {}", self.url).as_str());
         self.connections += 1;
         if self.connections >= 100 {
-            // a lot of connections!
             self.warn(format!("100 or more connections open!").as_str());
         }
     }
 }
 
+#[derive(Debug)]
+struct Product {
+    name: String,
+    price: u32
+}
+
 fn main() {
+    let laptop = Product { name: "MacBook Pro".to_owned(), price: 2000 };
+    buy_product(laptop, 20);
+
     let mut db = Database::new("localhost:5433".to_owned());
     db.connect();
-    //log_info!([TIME] starting program...);
+
+    log_info!([TIME] starting program...);
+}
+
+#[log_call(verbose)]
+fn buy_product(product: Product, discount: u32) {
+    // ...
 }
