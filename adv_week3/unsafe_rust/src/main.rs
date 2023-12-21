2dffa3a8871627aa386a4cf9055bc6f4dbf41f47
Emre Ergun <emreergun@engramsoft.com>
2023-12-21 21:19:15 +0300

    other unsafe opearions added as example

--- adv_week3/unsafe_rust/src/main.rs
+++ adv_week3/unsafe_rust/src/main.rs
@@ -1,11 +1,31 @@
-fn main() {
+// 3. Implement an unsafe trait
+unsafe trait MyTrait {
+    fn some_function(&self);
+}
+
+unsafe impl MyTrait for String {
+    fn some_function(&self) {
+        //..
+    }
+}
+
+// 4. Access/Modify a mutable static variable
+static mut COUNTER: u32 = 0;
+
+fn increment_counter() {
     unsafe {
+        COUNTER += 1;
+    }
+}
+
+fn main() {
+    //unsafe {
         // 1. Dereference a raw pointer
         // 2. Call an unsafe function
         // 3. Implement an unsafe trait
         // 4. Access/Modify a mutable static variable
         // 5. Access fields of a union.
-    }
+    //}
 
     // 1. Dereference a raw pointer
     let mut s = "Let's Get Rusty".to_owned();
@@ -13,10 +33,31 @@ fn main() {
     let r2 = &mut s as *mut String;
 
     let address = 0x012345usize;
-    let r3 = address as *const String;
+    let _r3 = address as *const String;
 
     unsafe {
         (*r2).push_str("!!!");
         println!("r1 is {}", *r1);
     }
+
+    // 2. Call an unsafe function
+    unsafe {
+        my_function();
+    }
+
+    // 3. Implement an unsafe trait
+    let s = "some string".to_owned();
+    s.some_function();
+
+    // 4. Access/Modify a mutable static variable
+    for _ in 0..10 {
+        increment_counter();
+    }
+    unsafe {
+        println!("counter:{COUNTER}");
+    }
+}
+
+unsafe fn my_function() {
+    println!("calling unsafe function: my_function");
 }
